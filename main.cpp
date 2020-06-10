3bcc0441757da4540febaa9e66e09145d99de908
coozoo <realcoozoo@gmail.com>
2020-06-10 23:38:03 +0300

    Improve translations search
    Improve logic to find out place of translations

--- main.cpp
+++ main.cpp
@@ -5,28 +5,54 @@
 #include <QApplication>
 #include <QTranslator>
 
+const QString APP_VERSION = "0.5b";
+
 int main(int argc, char *argv[])
 {
     QTextStream cout(stdout);
     QApplication a(argc, argv);
-    QTranslator translator;
-    cout<<translator.load(a.applicationDirPath()+"/lang/"+a.applicationName()+"_"+QLocale::system().name()+".qm")<<endl;
-    cout<<QLocale::system().name()<<endl;
-    a.installTranslator(&translator);
-    QString platform="";
-    Q_UNUSED(platform);
-    #if __GNUC__
-        #if __x86_64__
-            platform="-64bit";
-        #endif
-    #endif
-    a.setProperty("appversion","0.5b" + platform);
-    a.setProperty("appname","Android Process Monitor");
-    #ifdef Q_OS_LINUX
-        a.setWindowIcon(QIcon(":/images/appicon256.png"));
-    #endif
+    cout << QLocale::system().name() << endl;
+    QStringList translations;
+    QDir dir(a.applicationDirPath());
+    if (dir.cdUp() && dir.cd("share"))
+        {
+            translations.append(dir.absolutePath() + "/" + a.applicationName());
+        }
+    translations.append(QStandardPaths::standardLocations(QStandardPaths::DataLocation));
+    translations.append(QCoreApplication::applicationDirPath());
+    translations.append(a.applicationDirPath() + "/.qm");
+    translations.append(a.applicationDirPath() + "/lang");
+    QString translationFilePath = "";
+    cout << "Search for translations" << endl;
+    foreach (const QString &str, translations)
+        {
+            QFileInfo fileinfo(str + "/" + a.applicationName() + "_" + QLocale::system().name() + ".qm");
+            cout << fileinfo.filePath() << endl;
+            if (fileinfo.exists() && fileinfo.isFile())
+                {
+                    translationFilePath = fileinfo.filePath();
+                    cout << "Translation found in: " + translationFilePath << endl;
+                    break;
+                }
+        }
 
-    cout<<a.property("appname").toString()<<" "<<a.property("appversion").toString()<<endl;
+    QTranslator translator;
+    cout << translator.load(translationFilePath) << endl;
+    a.installTranslator(&translator);
+    QString platform = "";
+    Q_UNUSED(platform)
+#if __GNUC__
+#if __x86_64__
+    platform = "-64bit";
+#endif
+#endif
+    a.setProperty("appversion", APP_VERSION + platform);
+    a.setProperty("appname", "Android Process Monitor");
+#ifdef Q_OS_LINUX
+    a.setWindowIcon(QIcon(":/images/appicon256.png"));
+#endif
+
+    cout << a.property("appname").toString() << " " << a.property("appversion").toString() << endl;
     MainWindow w;
     w.setWindowTitle(a.property("appname").toString() + " " + a.property("appversion").toString());
     w.show();
