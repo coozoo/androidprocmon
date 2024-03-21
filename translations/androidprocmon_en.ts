0cf4a4a5c6c9b4d3f899406513de2f36d21356b8
happiness <realcoozoo@gmail.com>
2024-03-21 09:25:23 +0200

    update build system
    Update actions specs and other files
    a bit file rearrangment

---
+++ translations/androidprocmon_en.ts
@@ -0,0 +1,298 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Show about info</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Application targeted to make testers life easy :)&lt;br&gt;Monitor memory and CPU using top and dumpsys through ADB.&lt;br&gt;Log statistics to csv files, show charts etc...&lt;br&gt;&lt;br&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Creator: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Compiled with QT: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Runtime QT: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Compiler GCC: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>MinGW32: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>MinGW64: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Compiler clang: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Compiler msvc: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Project sources: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Charts Top</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Charts Dumpsys</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <source>Dialog</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Custom Path to ADB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Saved</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Select adb</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Files (adb*);;All Files (*)</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>adbExecute</name>
+    <message>
+        <source>SU</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Execute as root</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Execute Command in loop</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Use Loop</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Set Timeout to loop command</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Execute on device</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Reset console connection</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>adbViewer</name>
+    <message>
+        <source>Enable</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Start/Stop logcat</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Hide filter section</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Start match monitoring</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Save filter</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Delete current filter from saved</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>RegExp</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Use regexp</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter part of app name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Log</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enable/disable writing to file</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Timeout</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter app name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Refresh Devices (All activities will be stopped)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Logcat</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Process List</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Top Process Stat Log</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Dumpsys Proc meminfo</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Show filter section</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>mainContainer</name>
+    <message>
+        <source>Reset All Charts</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Hide Top</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Hide Dumpsys</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Range(min): </source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Flow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>After this time data on chart will move out so it will look like a flow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Save all charts as png images to the log folder</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Choose output format</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Set image width</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Set image height</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Show Top</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Show Dumpsys</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Saved</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/androidprocmon_uk_UA.ts
@@ -0,0 +1,309 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Show about info</source>
+        <translation>Показати інформацію про</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Про...</translation>
+    </message>
+    <message>
+        <source>Application targeted to make testers life easy :)&lt;br&gt;Monitor memory and CPU using top and dumpsys through ADB.&lt;br&gt;Log statistics to csv files, show charts etc...&lt;br&gt;&lt;br&gt;</source>
+        <translation>Прикладка призначена для полегшення життя тестерів :) &lt;br&gt;Для моніторингу пам&apos;яті та процесора за допомогою top та dumpsys використовуючи ADB&lt;br&gt;Логування статистики в csv файл, відображення графіків та ін... &lt;br&gt;&lt;br&gt;</translation>
+    </message>
+    <message>
+        <source>Creator: </source>
+        <translation>Створено: </translation>
+    </message>
+    <message>
+        <source>Compiled with QT: </source>
+        <translation>Зібрано в QT: </translation>
+    </message>
+    <message>
+        <source>Runtime QT: </source>
+        <translation>Середовище QT: </translation>
+    </message>
+    <message>
+        <source>Compiler GCC: </source>
+        <translation>Компілятор GCC: </translation>
+    </message>
+    <message>
+        <source>MinGW32: </source>
+        <translation>MinGW32: </translation>
+    </message>
+    <message>
+        <source>MinGW64: </source>
+        <translation>MinGW64: </translation>
+    </message>
+    <message>
+        <source>Compiler clang: </source>
+        <translation>Компілятор clang: </translation>
+    </message>
+    <message>
+        <source>Compiler msvc: </source>
+        <translation>Компілятор msvc: </translation>
+    </message>
+    <message>
+        <source>Project sources: </source>
+        <translation>Сирці проекта: </translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Налаштування</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Charts Top</source>
+        <translation>Графіки Top</translation>
+    </message>
+    <message>
+        <source>Charts Dumpsys</source>
+        <translation>Графіки Dumpsys</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <source>Dialog</source>
+        <translation>Діалог</translation>
+    </message>
+    <message>
+        <source>Custom Path to ADB</source>
+        <translation>Вказати шлях до ADB</translation>
+    </message>
+    <message>
+        <source>...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Інфо</translation>
+    </message>
+    <message>
+        <source>Saved</source>
+        <translation>Збережено</translation>
+    </message>
+    <message>
+        <source>Select adb</source>
+        <translation>Обрати adb</translation>
+    </message>
+    <message>
+        <source>Files (adb*);;All Files (*)</source>
+        <translation>Файли (adb*);;Всі файли (*)</translation>
+    </message>
+</context>
+<context>
+    <name>adbExecute</name>
+    <message>
+        <source>SU</source>
+        <translation>SU</translation>
+    </message>
+    <message>
+        <source>Execute as root</source>
+        <translation>Виконати як root</translation>
+    </message>
+    <message>
+        <source>Execute Command in loop</source>
+        <translation>Виконувати команду в циклі</translation>
+    </message>
+    <message>
+        <source>Use Loop</source>
+        <translation>Зациклити команду</translation>
+    </message>
+    <message>
+        <source>Set Timeout to loop command</source>
+        <translation>Інтервал для циклу</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Виконати</translation>
+    </message>
+    <message>
+        <source>Execute on device</source>
+        <translation>Виконати команду на пристрої</translation>
+    </message>
+    <message>
+        <source>Reset console connection</source>
+        <translation>Скинути консоль</translation>
+    </message>
+</context>
+<context>
+    <name>adbViewer</name>
+    <message>
+        <source>Enable</source>
+        <translation>Увімкнути</translation>
+    </message>
+    <message>
+        <source>Start/Stop logcat</source>
+        <translation>Запустити/припинити logcat</translation>
+    </message>
+    <message>
+        <source>Hide filter section</source>
+        <translation>Сховати секцію фільтра</translation>
+    </message>
+    <message>
+        <source>Start match monitoring</source>
+        <translation>Почати моніторинг наявності співпадінь</translation>
+    </message>
+    <message>
+        <source>Save filter</source>
+        <translation>Зберегти фільтр</translation>
+    </message>
+    <message>
+        <source>Delete current filter from saved</source>
+        <translation>Видалити обраний фільтр зі збережених</translation>
+    </message>
+    <message>
+        <source>RegExp</source>
+        <translation>RegExp</translation>
+    </message>
+    <message>
+        <source>Use regexp</source>
+        <translation>Використовувати регулярний вираз</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Оновити</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>Очистити</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Старт</translation>
+    </message>
+    <message>
+        <source>Enter part of app name</source>
+        <translation>Введіть частину імені застосунку</translation>
+    </message>
+    <message>
+        <source>Log</source>
+        <translation>Лог</translation>
+    </message>
+    <message>
+        <source>Enable/disable writing to file</source>
+        <translation>Включити/виключити запис до файлу</translation>
+    </message>
+    <message>
+        <source>Timeout</source>
+        <translation>Інтервал</translation>
+    </message>
+    <message>
+        <source>Enter app name</source>
+        <translation>Введіть назву застосунку</translation>
+    </message>
+    <message>
+        <source>Refresh Devices (All activities will be stopped)</source>
+        <translation>Оновити список пристроїв (Всі завдання будуть зупинені)</translation>
+    </message>
+    <message>
+        <source>Logcat</source>
+        <translation>Logcat</translation>
+    </message>
+    <message>
+        <source>Execute</source>
+        <translation>Виконати</translation>
+    </message>
+    <message>
+        <source>Process List</source>
+        <translation>Список процесів</translation>
+    </message>
+    <message>
+        <source>Top Process Stat Log</source>
+        <translation>Моніторинг процесу top</translation>
+    </message>
+    <message>
+        <source>Dumpsys Proc meminfo</source>
+        <translation>Моніторинг процесу dumpsys</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Зупинити</translation>
+    </message>
+    <message>
+        <source>Show filter section</source>
+        <translation>Показати секцію фільтра</translation>
+    </message>
+</context>
+<context>
+    <name>mainContainer</name>
+    <message>
+        <source>Reset All Charts</source>
+        <translation>Скинути всі графіки</translation>
+    </message>
+    <message>
+        <source>Hide Top</source>
+        <translation>Сховати секцію top</translation>
+    </message>
+    <message>
+        <source>Hide Dumpsys</source>
+        <translation>Сховати секцію dumpsys</translation>
+    </message>
+    <message>
+        <source>Range(min): </source>
+        <translation>Діапазон(хв): </translation>
+    </message>
+    <message>
+        <source>Flow</source>
+        <translation>Потік</translation>
+    </message>
+    <message>
+        <source>After this time data on chart will move out so it will look like a flow</source>
+        <translation>Після цього часу старі данні на графіках будуть видалятись та рухатись як потік</translation>
+    </message>
+    <message>
+        <source>Save all charts as png images to the log folder</source>
+        <translation>Зберегти всі графіки як png зображення до теки логування</translation>
+    </message>
+    <message>
+        <source>Choose output format</source>
+        <translation>Вибрати формат виводу</translation>
+    </message>
+    <message>
+        <source>Set image width</source>
+        <translation>Встановити ширину зображення</translation>
+    </message>
+    <message>
+        <source>Set image height</source>
+        <translation>Встановити зображення висоти</translation>
+    </message>
+    <message>
+        <source>Show Top</source>
+        <translation>Показати секцію top</translation>
+    </message>
+    <message>
+        <source>Show Dumpsys</source>
+        <translation>Показати секцію dumpsys</translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation>Інфо</translation>
+    </message>
+    <message>
+        <source>Saved</source>
+        <translation>Збережено</translation>
+    </message>
+</context>
+<context>
+    <name>mainContainerForm</name>
+    <message>
+        <source>ADB manager</source>
+        <translation type="obsolete">ADB менеджер</translation>
+    </message>
+    <message>
+        <source>Charts Manager</source>
+        <translation type="obsolete">Менеджер графіків</translation>
+    </message>
+</context>
+</TS>
